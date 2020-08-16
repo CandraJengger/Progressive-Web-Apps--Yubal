0144f0be9dbc9877dbc3d5734b7d43d0b184e678
Dimas Aldi Candra S <cakjengger@gmail.com>
2020-08-16 19:51:10 +0700

    Update push.js

--- push.js
+++ push.js
@@ -2,8 +2,8 @@
 const webPush = require('web-push')
 
 const vapidKeys = {
-    "publicKey": "BIX--w4j365a_mvKq4dSBLJ9LC8mOu7eK4EtPrJSdr1R4f4-x9nQmcVGOOqmKW5kYZUDdYbuKrbileyGH7NFuSw",
-    "privateKey": "kzblCv5vpkFJIe07hRc1zMzrnHKBKyPHdeQbYKsWFu0"
+    "publicKey": "<Your key>",
+    "privateKey": "<Your key>"
 }
   
 webPush.setVapidDetails(
@@ -14,16 +14,16 @@ webPush.setVapidDetails(
 
 
 const pushSubscription = {
-    "endpoint": "https://fcm.googleapis.com/fcm/send/fWmrMNThe6A:APA91bFIlhjlSoszZt6C_Y2sdS7p-UK11tY950mt_cpmHJUGSvkipzpFGXnTJJaCVxytvnetF7FMK4pGPAmW_LPfyG2snpp0LKIuvB2Z6LO8JV5O2tbUn05N39qjoS_5I3BfzcGyKrI0",
+    "endpoint": "<Your End Point>",
     "keys": {
-        "p256dh": "BIjSFXLAEdgKYLLUuT0KU6Si7ZQV3/sSC/YJ3y5Q+Poc+PbX0+sUtybjOYbkSu6F8L+8BKQbilbY5Fn8EAXJTsA=",
-        "auth": "QUk/M6I4aEW1IVyV/Lr0yA=="
+        "p256dh": "<Your key>"",
+        "auth": "<Your key>"
     }
 }
 
 const payload = 'Tonight! Champions league final, Chelsea vs Arsenal at the San Siro Stadium, Italy'
 const options = {
-    gcmAPIKey: '541964131155',
+    gcmAPIKey: '<Your gcmAPIKey>',
     TTL: 60
 }
 
